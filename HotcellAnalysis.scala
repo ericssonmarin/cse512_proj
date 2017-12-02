228291797ab205692f1a1ea17b1c2c5450e02cd4
ericssonmarin <ericssonmarin@yahoo.com.br>
2017-12-01 19:19:31 -0800

    Add files via upload

--- HotcellAnalysis.scala
+++ HotcellAnalysis.scala
@@ -43,25 +43,28 @@ object HotcellAnalysis {
 
     //The implementation starts here
 
-    //println("Creating the full list of cells")
+    println("Creating the full list of cells")
 
     val cells_full = pickupInfo.collect() //grabing all existing cells
 
-    //println("Creating the distinct list of cells")
+    println("Creating the distinct list of cells")
 
     val cells_1 = cells_full.distinct //grabing all existing cells
 
-    //println("Number of Cells" + cells_1.length.toString)
+    println("Number of Cells" + cells_1.length.toString)
 
-    //println("Creating the second distinct list of cells")
+    println("Creating the second distinct list of cells")
 
     val cells_2 = cells_1.clone() //making a copy of the distinct cells
 
+    var point_1 = new Array[Int](2)
+    var point_2 = new Array[Int](2)
     var feature: Long = 0
     var accumulatedFeature: Long = 0
     var accumulatedWeights: Long = 0
     var x_bar: Double = 0.0
     var x_square: Double = 0.0
+    var feature_sum_values: Double = 0.0
     var s: Double = 0.0
     var g_numerador: Double = 0.0
     var g_denominator: Double = 0.0
@@ -70,7 +73,7 @@ object HotcellAnalysis {
     var lstData : List[(Int,Int,Int,Double)] = List()
     var lstDataFinal : List[(Int,Int,Int)] = List()
 
-    //println("Calculating the features")
+    println("Calculating the features")
 
     var i: Integer = 0
 
@@ -78,9 +81,9 @@ object HotcellAnalysis {
 
     for (cell <- cells_full) {
 
-      //i += 1
+      i += 1
 
-      //println("iteration: " + i.toString + " out of " + cells_full.length.toString)
+      println("iteration: " + i.toString + " out of " + cells_full.length.toString)
 
       if (features.contains((cell.get(0).toString, cell.get(1).toString, cell.get(2).toString))) {
         features((cell.get(0).toString, cell.get(1).toString, cell.get(2).toString)) += 1
@@ -104,13 +107,13 @@ object HotcellAnalysis {
 
     println("Iterating over the cells")
 
-    //i = 0
+    i = 0
 
     for (cell_1 <- cells_1) {
 
-      //i += 1
+      i += 1
 
-      //println("iteration: " + i.toString + " out of " + cells_1.length.toString)
+      println("iteration: " + i.toString + " out of " + cells_1.length.toString)
 
       accumulatedFeature = 0
       accumulatedWeights = 0
@@ -124,34 +127,34 @@ object HotcellAnalysis {
 
         //if (cell_1.toString != cell_2.toString) { no need this, since the exactly same cell should be included in the calculations
 
-        if (cell_1.get(2).toString.toInt == (cell_2.get(2).toString.toInt - 1) || cell_1.get(2).toString.toInt == cell_2.get(2).toString.toInt || cell_1.get(2).toString.toInt == (cell_2.get(2).toString.toInt + 1)) {
+          if (cell_1.get(2).toString.toInt == (cell_2.get(2).toString.toInt - 1) || cell_1.get(2).toString.toInt == cell_2.get(2).toString.toInt || cell_1.get(2).toString.toInt == (cell_2.get(2).toString.toInt + 1)) {
 
-          if (cell_1.get(0).toString.toInt == (cell_2.get(0).toString.toInt - 1) || cell_1.get(0).toString.toInt == cell_2.get(0).toString.toInt || cell_1.get(0).toString.toInt == (cell_2.get(0).toString.toInt + 1)) {
+            if (cell_1.get(0).toString.toInt == (cell_2.get(0).toString.toInt - 1) || cell_1.get(0).toString.toInt == cell_2.get(0).toString.toInt || cell_1.get(0).toString.toInt == (cell_2.get(0).toString.toInt + 1)) {
 
-            if (cell_1.get(1).toString.toInt == (cell_2.get(1).toString.toInt - 1) || cell_1.get(1).toString.toInt == cell_2.get(1).toString.toInt || cell_1.get(1).toString.toInt == (cell_2.get(1).toString.toInt + 1)) {
+              if (cell_1.get(1).toString.toInt == (cell_2.get(1).toString.toInt - 1) || cell_1.get(1).toString.toInt == cell_2.get(1).toString.toInt || cell_1.get(1).toString.toInt == (cell_2.get(1).toString.toInt + 1)) {
 
-              feature = features((cell_2.get(0).toString, cell_2.get(1).toString, cell_2.get(2).toString))
+                feature = features((cell_2.get(0).toString, cell_2.get(1).toString, cell_2.get(2).toString))
 
-              accumulatedFeature += feature
-              accumulatedWeights += 1
+                accumulatedFeature += feature
+                accumulatedWeights += 1
 
               //} else {
 
               //  println("longitude fails!")
 
-            }
+              }
 
             //} else {
 
             //  println("latitude fails!")
 
-          }
+            }
 
           //} else {
 
           //  println("date fails!")
 
-        }
+          }
 
         //} else {
 
@@ -171,11 +174,11 @@ object HotcellAnalysis {
 
         lstData = lstData:+((cell_1.get(0).toString.toInt,cell_1.get(1).toString.toInt,cell_1.get(2).toString.toInt,getis_ord.toString.toDouble))
 
-      }
+    }
 
     }
 
-    //println("Dumping the cells in a dataframe")
+    println("Dumping the cells in a dataframe")
 
     var lstToDf = spark.sparkContext.parallelize(lstData).toDF("lat", "long", "date", "getis_ord")
 
